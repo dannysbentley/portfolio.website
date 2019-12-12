a665ebe44c44e456a0a6c834e8ef4b6047d63516
Danny Bentley <danny.bentley@som.com>
2019-12-12 07:34:48 -0800

    Updates

--- js/main.js
+++ js/main.js
@@ -49,3 +49,45 @@ function toggleControls() {
     video.setAttribute("controls", "controls");
   }
 }
+
+function validation() {
+  var name = document.getElementById("name").value;
+  var subject = document.getElementById("subject").value;
+  var phone = document.getElementById("phone").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+  var error_message = document.getElementById("error_message");
+  var text;
+
+  error_message.style.padding = "10px";
+
+  if (name.length < 5) {
+    text = "Please enter valid name";
+    error_message.innerHTML = text;
+    return false;
+  }
+
+  if (subject.length < 4) {
+    text = "Please enter valid subject";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if (isNaN(phone) || phone.length < 10) {
+    text = "Please enter valid phone number";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if (email.indexOf("@") == -1 || phone.length < 6) {
+    text = "Please enter valid email";
+    error_message.innerHTML = text;
+    return false;
+  }
+
+  if (message.length <= 140) {
+    text = "Please enter more than 140 characters";
+    error_message.innerHTML = text;
+    return false;
+  }
+  alert("form has been submitted sucessfully");
+  return false;
+}
